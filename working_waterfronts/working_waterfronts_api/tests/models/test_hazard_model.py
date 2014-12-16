5d0387ac496d8f71dd2b4766ea89f11a3064dfdb
Evan Tschuy <tschuy@osuosl.org>
2014-12-16 10:05:23 -0800

    Hazard model test

---
+++ working_waterfronts/working_waterfronts_api/tests/models/test_hazard_model.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from working_waterfronts.working_waterfronts_api.models import Hazard
+from django.contrib.gis.db import models
+
+
+class HazardTestCase(TestCase):
+
+    def setUp(self):
+        self.expected_fields = {
+            'name': models.TextField,
+            'description': models.TextField,
+            'created': models.DateTimeField,
+            'modified': models.DateTimeField,
+            'id': models.AutoField
+        }
+
+    def test_fields_exist(self):
+        model = models.get_model('working_waterfronts_api', 'Hazard')
+        for field, field_type in self.expected_fields.items():
+            self.assertEqual(
+                field_type, type(model._meta.get_field_by_name(field)[0]))
+
+    def test_no_additional_fields(self):
+        fields = Hazard._meta.get_all_field_names()
+        self.assertEqual(sorted(fields), sorted(self.expected_fields.keys()))
+
+    def test_created_modified_fields(self):
+        self.assertTrue(Hazard._meta.get_field('modified').auto_now)
+        self.assertTrue(Hazard._meta.get_field('created').auto_now_add)
+
+    def test___unicode___method(self):
+        assert hasattr(Hazard, '__unicode__'), "No __unicode__ method found"
