972ea39c5482f2ce6db49060c94791b171617b63
Evan Tschuy <tschuy@osuosl.org>
2014-12-16 10:06:50 -0800

    Category model test

---
+++ working_waterfronts/working_waterfronts_api/tests/models/test_category_model.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from working_waterfronts.working_waterfronts_api.models import Category
+from django.contrib.gis.db import models
+
+
+class CategoryTestCase(TestCase):
+
+    def setUp(self):
+        self.expected_fields = {
+            'category': models.TextField,
+            'pointofinterestcategory': models.related.RelatedObject,
+            'id': models.AutoField
+        }
+
+    def test_fields_exist(self):
+        model = models.get_model('working_waterfronts_api', 'Category')
+        for field, field_type in self.expected_fields.items():
+            self.assertEqual(
+                field_type, type(model._meta.get_field_by_name(field)[0]))
+
+    def test_no_additional_fields(self):
+        fields = Category._meta.get_all_field_names()
+        self.assertEqual(sorted(fields), sorted(self.expected_fields.keys()))
+
+    def test_created_modified_fields(self):
+        self.assertTrue(Category._meta.get_field('modified').auto_now)
+        self.assertTrue(Category._meta.get_field('created').auto_now_add)
+
+    def test___unicode___method(self):
+        assert hasattr(Category, '__unicode__'), "No __unicode__ method found"
