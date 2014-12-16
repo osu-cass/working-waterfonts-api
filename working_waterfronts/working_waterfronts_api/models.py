7c5301275245550b5cbede11eb26945ae1bdbead
Evan Tschuy <tschuy@osuosl.org>
2014-12-16 10:01:24 -0800

    Add Video model, update test

--- working_waterfronts/working_waterfronts_api/models.py
+++ working_waterfronts/working_waterfronts_api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    """
+    The video model holds a video URL and related data.
+
+    The Created and Modified time fields are created automatically by
+    Django when the object is created or modified, and can not be altered.
+
+    This model uses Django's built-ins for holding the video URL and
+    data in the database, as well as for keeping created and modified
+    timestamps.
+    """
+
+    def __unicode__(self):
+        return self.caption
+
+    video = models.URLField()
+    caption = models.TextField(blank=True)
+    name = models.TextField(default='')
+    pointofinterest = models.ForeignKey('PointOfInterest')
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def natural_key(self):
+        return {
+            'caption': self.caption,
+            'name': self.name,
+            'link': self.video
+        }
---
+++ working_waterfronts/working_waterfronts_api/tests/models/test_video_model.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+
+from working_waterfronts.working_waterfronts_api.models import Video
+from django.contrib.gis.db import models
+
+
+class VideoTestCase(TestCase):
+    def setUp(self):
+        self.expected_fields = {
+            'video': models.URLField,
+            'caption': models.TextField,
+            'name': models.TextField,
+            'created': models.DateTimeField,
+            'modified': models.DateTimeField,
+            'pointofinterest': models.ForeignKey,
+            'pointofinterest_id': models.ForeignKey,
+            'id': models.AutoField
+        }
+
+        self.optional_fields = {
+            'caption'
+        }
+
+    def test_fields_exist(self):
+        for field, field_type in self.expected_fields.items():
+            self.assertEqual(
+                field_type, type(Video._meta.get_field_by_name(field)[0]))
+
+    def test_no_additional_fields(self):
+        fields = Video._meta.get_all_field_names()
+        self.assertEqual(sorted(fields), sorted(self.expected_fields.keys()))
+
+    def test_created_modified_fields(self):
+        self.assertTrue(Video._meta.get_field('modified').auto_now)
+        self.assertTrue(Video._meta.get_field('created').auto_now_add)
+
+    def test_optional_fields(self):
+        for field in self.optional_fields:
+            self.assertEqual(
+                Video._meta.get_field_by_name(field)[0].blank, True)
+
+    def test___unicode___method(self):
+        assert hasattr(Video, '__unicode__'), "No __unicode__ method found"
