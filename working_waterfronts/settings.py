5c8dbd6ab6add1f8b0847c37d9ffd174260ec71d
Evan Tschuy <tschuy@osuosl.org>
2015-01-15 16:42:26 -0800

    Add environmental/file-based settings

--- working_waterfronts/settings.py
+++ working_waterfronts/settings.py
@@ -1,83 +1,18 @@
-"""
-Django settings for working_waterfronts project.
+# flake8: noqa
+# This module is how we import settings, and override settings with various
+# precedences.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
+# First our base.py settings module is imported, with all of the
+# important defaults.
+#
+# Next our yaml file is opened, read, and settings defined in the yaml config
+# may override settings already defined.
 
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from .base import *
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fuo^k%$e4^qg9$77z7z(c$%sh5p0v4+f&8vosmkbk+biqk*)1@'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'working_waterfronts.urls'
-
-WSGI_APPLICATION = 'working_waterfronts.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
+if os.environ.get('ENVIRONMENTCONFIG'):
+    from working_waterfronts.environment_config import *
+else:
+    from .yaml_config import *
