0b64ed4c7a402cb1fad0f9f1e1298e111fa7a30c
Evan Tschuy <tschuy@osuosl.org>
2015-01-22 15:49:35 -0800

    Add urls to urls.py

--- working_waterfronts/urls.py
+++ working_waterfronts/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'working_waterfronts.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    (r'^', include('working_waterfronts.working_waterfronts_api.urls'))
-)
+    (r'^', include('working_waterfronts.working_waterfronts_api.urls')),
+    )
+
+if settings.DEBUG:
+    # Allow statics to be served if in debug mode
+    urlpatterns += patterns(
+        '',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT}))
--- working_waterfronts/working_waterfronts_api/urls.py
+++ working_waterfronts/working_waterfronts_api/urls.py
@@ -1,6 +1,18 @@
-# from django.conf.urls import url
 from django.conf.urls import patterns
+from django.conf.urls import url
 
 urlpatterns = patterns(
-    ''
+    '',
+    url(r'^entry/hazards/new/?$',
+        'working_waterfronts.working_waterfronts_api.views.entry.hazards.hazard',
+        name='new-hazard'),
+
+    url(r'^entry/hazards/(?P<id>\d+)/?$',
+        'working_waterfronts.working_waterfronts_api.views.entry.hazards.hazard',
+        name='edit-hazard'),
+
+    url(r'^entry/hazards/?$',
+        'working_waterfronts.working_waterfronts_api.views.entry.hazards.list',
+        name='entry-list-hazards'),
+
 )
