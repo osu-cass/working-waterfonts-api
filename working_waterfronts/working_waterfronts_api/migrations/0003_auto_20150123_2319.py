15abd965f706a14f3255a1e95a59ad7325b0a710
Evan Tschuy <tschuy@osuosl.org>
2015-01-23 15:19:32 -0800

    Fix migrations

---
+++ working_waterfronts/working_waterfronts_api/migrations/0003_auto_20150123_2319.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('working_waterfronts_api', '0002_auto_20150121_2106'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='hazard',
+            name='pointofinterests',
+            field=models.ManyToManyField(to='working_waterfronts_api.PointOfInterest', null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
