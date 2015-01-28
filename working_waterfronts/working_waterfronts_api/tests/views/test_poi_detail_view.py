1c452475ac1890701004c2c74349d4af1d87a368
MaraJade <catbookgirl@gmail.com>
2015-01-28 14:20:53 -0800

    Created test and urls file
    refs #17871

---
+++ working_waterfronts/working_waterfronts_api/tests/views/test_poi_detail_view.py
@@ -0,0 +1,88 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+
+import json
+
+
+class POIViewTestCase(TestCase):
+    fixtures = ['test_fixtures']
+
+    def setUp(self):
+        user = User.objects.create_user(username='test', password='pass')
+        admin_group = Group(name='Administration Users')
+        admin_group.save()
+        user.groups.add(admin_group)
+        self.client.post(
+            reverse('login'), {'username': 'test', 'password': 'pass'})
+
+        self.expected_poi = """
+{
+  "error": {
+    "status": false,
+    "name": null,
+    "text": null,
+    "debug": null,
+    "level": null
+  },
+  "id": 1,
+  "name": "Newport Lighthouse",
+  "alt_name": "",
+  "description": "A pretty nice lighthouse",
+  "history": "It was built at some time in the past",
+  "facts": "It's a lighthouse",
+  "location": "POINT (-124.1053399999999982 43.9668739999999971)",
+  "street": "123 Fake St",
+  "city": "Newport",
+  "state": "Oregon",
+  "location_description": "out on the cape over there",
+  "zip": "11234",
+  "website": "",
+  "email": "",
+  "phone": "",
+  "hazards": [1],
+  "categories": [1],
+  "images": [1],
+  "videos": [1],
+  "created": "2014-08-08 23:27:05.568395+00:00",
+  "modified": "2014-08-08 23:27:05.568395+00:00"
+}"""
+
+        self.expected_not_found = """
+{
+  "error": {
+    "status": true,
+    "text": "POI id 999 was not found.",
+    "name": "POI Not Found",
+    "debug": "DoesNotExist: POI matching query does not exist.",
+    "level": "Error"
+  }
+}"""
+
+    def test_url_endpoint(self):
+        url = reverse('poi-details', kwargs={'id': '1'})
+        self.assertEqual(url, '/1/poi/1')
+
+    def test_known_poi(self):
+        response = self.client.get(
+            reverse('poi-details', kwargs={'id': '1'}))
+
+        parsed_answer = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+
+        expected_answer = json.loads(self.expected_poi)
+        self.maxDiff = None
+
+        self.assertEqual(parsed_answer, expected_answer)
+
+    def test_poi_not_found(self):
+        response = self.client.get(
+            reverse('poi-details', kwargs={'id': '999'}))
+        parsed_answer = json.loads(response.content)
+        self.assertEqual(response.status_code, 404)
+
+        expected_answer = json.loads(self.expected_not_found)
+        self.maxDiff = None
+
+        self.assertEqual(parsed_answer, expected_answer)
--- working_waterfronts/working_waterfronts_api/urls.py
+++ working_waterfronts/working_waterfronts_api/urls.py
@@ -1,6 +1,12 @@
-# from django.conf.urls import url
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
-    ''
+    '',
+    url(r'^login/?$',
+        'working_waterfronts.working_waterfronts_api.views.entry.login.login_user',
+        name='login'),
+
+    url(r'^1/poi/(?P<id>\d+)/?$',
+        'working_waterfronts.working_waterfronts_api.views.poi.poi_details',
+        name='poi-details'),
 )
