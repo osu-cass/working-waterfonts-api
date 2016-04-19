4c04cea300137aeb994314c052ccb8dcdd1480b2
Ken Lett <kennric@osuosl.org>
2016-04-19 15:28:26 -0700

    update tests for lat/long field updates

--- working_waterfronts/working_waterfronts_api/views/entry/pois.py
+++ working_waterfronts/working_waterfronts_api/views/entry/pois.py
@@ -88,7 +88,8 @@ def poi(request, id=None):
         try:
             if post_data['latitude'] and post_data['longitude']:
                 post_data['location'] = fromstr('POINT(%s %s)' %
-                    (post_data['longitude'], post_data['latitude']), srid=4326)
+                    (post_data['longitude'], post_data['latitude']), 
+                    srid=4326)
 
             else:
                 coordinates = coordinates_from_address(
@@ -190,16 +191,9 @@ def poi(request, id=None):
         poi.longitude = poi.location[0]
         title = "Edit {0}".format(poi.name)
         post_url = reverse('edit-poi', kwargs={'id': id})
-        poi_form = PointOfInterestForm(instance=poi, initial={'latitude': poi.latitude, 
-            'longitude': poi.longitude})
-        """
-        if post_data:
-            poi_form.fields['latitude'] = post_data['latitude']
-            poi_form.fields['longitude'] = post_data['longitude']
-        else:
-            poi_form.fields['latitude'] = poi.location[1]
-            poi_form.fields['longitude'] = poi.location[0]
-        """
+        poi_form = PointOfInterestForm(instance=poi, 
+            initial={'latitude': poi.latitude, 'longitude': poi.longitude})
+
         existing_images = poi.images.all()
         existing_videos = poi.videos.all()
         existing_categories = poi.categories.all()
